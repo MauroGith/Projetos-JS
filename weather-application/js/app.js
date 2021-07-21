d22764900e9e675aa4b6de84e408a62ed54f5766
MauroGith <78665680+MauroGith@users.noreply.github.com>
2021-07-21 16:39:51 -0300

    Update app.js

--- weather-application/js/app.js
+++ weather-application/js/app.js
@@ -6,37 +6,28 @@ const cityCard = document.querySelector('[data-js="city-card"]')
 const timeImg = document.querySelector('[data-js="time"]')
 const timeIconContainer = document.querySelector('[data-js="time-icon"]')
 
-const showCard = () => 
-   cityCard.classList.contains('d-none')
-      ? cityCard.classList.remove('d-none')
-      : cityCard
+const showCityCard = () => {
+   if(cityCard.classList.contains('d-none')){
+      cityCard.classList.remove('d-none')
+   }
+}
 
-const attDisplayImage = IsDayTime => 
-  IsDayTime 
-   ? timeImg.src = './src/dia.jpg'
-   : timeImg.src = './src/noite.jpg' 
-
-const attDisplayText = (timeIcon, LocalizedName, WeatherText, Temperature) => {
-   timeIconContainer.innerHTML = timeIcon
+const showCityWeatherInfo = async event => {
+   const inputValue = event.target.city.value
+   const [{ Key, LocalizedName }] = await getCityData(inputValue)
+   const [{ WeatherText, Temperature, IsDayTime, WeatherIcon }] = await getCityWeather(Key)
+   
+   timeImg.src = IsDayTime ? './src/dia.jpg' : './src/noite.jpg'
+   timeIconContainer.innerHTML = `<img src="./src/icons/${WeatherIcon}.svg"/>`
    cityName.textContent = LocalizedName
    cityWeather.textContent = WeatherText
    cityTemperature.textContent = Temperature.Metric.Value
-}
-
-const APIData = async event => {
-   const inputValue = event.target.city.value
-   const [{ Key, LocalizedName }] = await getCityData(inputValue)
-   const [{WeatherText, Temperature, IsDayTime, WeatherIcon}] = await getCityWeather(Key)
-   const timeIcon = `<img src="./src/icons/${WeatherIcon}.svg"/>`
-
-   attDisplayImage(IsDayTime)
-   attDisplayText(timeIcon, LocalizedName, WeatherText, Temperature)
+   showCityCard()
 }
 
 cityForm.addEventListener('submit', event => {
    event.preventDefault()
-   showCard()
-   APIData(event)
+   showCityWeatherInfo(event)
    
    cityForm.reset()
 })
